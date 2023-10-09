ea50f04f6b697f13e0d47301ea0e31806de4c51b
oshan <oshxn998@gmail.com>
2023-10-09 09:05:43 +0530

    10/9 monday commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements: any[] =[];
-  newServerName='';
-  newServerContent='';
+  serverElements: any[] =[{type: 'server', name: 'testserver', content:'just a test'}];
 
-  onAddServer(){
+  onServerAdded(serverData: {serverName: string,serverContent: string}){
     this.serverElements.push({
       type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: serverData.serverName,
+      content: serverData.serverContent
     });
   }
 
-  onAddBlueprint(){
+  onBlueprintAdded(blueprintData: {serverName: string,serverContent: string}){
     this.serverElements.push({
       type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
     });
   }
+
 }
